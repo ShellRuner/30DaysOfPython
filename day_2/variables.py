00edddb665f56641318edb2b918939eafb04b046
ShellRuner <sitouamu510@gmail.com>
2025-07-24 15:24:02 +0000

    day 2 of 30 days of python

---
+++ day_2/variables.py
@@ -0,0 +1,79 @@
+#Day 2: 30 Days of python progamming
+#Level 1
+
+first_name = "Paul"
+last_name = "DOE"
+full_name = "Paul Doe"
+country = "Togo"
+city = "Lome"
+age = 12
+year = 2025
+is_married = False
+is_true = True
+is_light_on = "Yes"
+A, B, C = 10, 'School', True
+
+#Level 2
+#Question 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(A))
+print(type(B))
+print(type(C))
+
+#Question 2
+print(len(first_name))
+
+#Question 3
+print(max(len(first_name), len(last_name)))
+
+#Question 4
+num_one = 5
+num_two = 4
+
+#Question 5
+total = num_one + num_two
+
+#Question 6
+diff = num_two - num_one
+
+#Question 7
+product = num_two * num_one
+
+#Question 8
+division = num_one / num_two
+
+#Question 9
+remainder = num_two % num_one
+
+#Question 10
+exp = num_one ** num_two
+
+#Question 11
+floor_division = num_one / num_two
+
+#Question 12
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+
+circum_of_circle = 2 * 3.14 * radius
+
+radius = int(input("Enter the radius: "))
+area_of_circle = 3.14 * radius ** 2
+
+#Question 13
+first_name = str(input("Enter you first name: "))
+last_name = str(input("Enter you last name: "))
+country = str(input("Enter your country name: "))
+age = int(input("Enter your age: "))
+
+#Question 14
+print(help('keywords'))
